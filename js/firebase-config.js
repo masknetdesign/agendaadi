68708d0f5ca0ee4a4f78e7eb7329ee3698f9ffeb
masknetdesign <masknetdesign@gmail.com>
2025-04-04 21:10:59 -0300

    Configurar Firebase para GitHub Pages

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBkrBLKuklZgPm1nz2G997ULiYycZMb9F8",
+    authDomain: "avisoseeventos.firebaseapp.com",
+    databaseURL: "https://avisoseeventos-default-rtdb.firebaseio.com",
+    projectId: "avisoseeventos",
+    storageBucket: "avisoseeventos.appspot.com",
+    messagingSenderId: "247706769451",
+    appId: "1:247706769451:web:ce31cd9d0ca22cd267b26e",
+    measurementId: "G-QE1Z4RQ60T"
+};
+
+// Exporta a configuração
+window.firebaseConfig = firebaseConfig; 
